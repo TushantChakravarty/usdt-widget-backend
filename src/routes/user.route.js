1d74b1794ff6ea44c3eb54fd2e2448c900479b7a
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-08-27 15:51:55 +0530

    Revert "jwt plugin setup"
    This reverts commit c8109897631ce2b4cc71c708d2351cc327395b09.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -15,82 +15,72 @@ const { User: user } = Controller;
 const { User: validator } = Validator;
 
 const routes = async (route, options) => { // route = fastify instance
-  /**
-   * Route for user login.
-   * Handles the user login functionality.
-   */
-  route.post('/login', {
-    schema: validator.login,
-    preHandler: async (request, reply) => {
-      request.body.emailId = request.body.emailId.trim();
-      request.body.password = request.body.password.trim();
-    },
-    handler: user.login,
-  });
+    /**
+     * Route for user login.
+     * Handles the user login functionality.
+     */
+    route.post('/login', {
+        schema: validator.login,
+        preHandler: async (request, reply) => {
+            request.body.emailId = request.body.emailId.trim();
+            request.body.password = request.body.password.trim();
+        },
+        handler: user.login,
+    });
 
-  /**
-   * Route for user signup.
-   * Handles the user signup functionality.
-   */
-  route.post('/signup', {
-    schema: validator.signup,
-    preHandler: async (request, reply) => {
-      request.body.emailId = request.body.emailId.trim();
-      request.body.password = request.body.password.trim();
-    },
-    handler: user.signup,
-  });
+    /**
+     * Route for user signup.
+     * Handles the user signup functionality.
+     */
+    route.post('/signup', {
+        schema: validator.signup,
+        preHandler: async (request, reply) => {
+            request.body.emailId = request.body.emailId.trim();
+            request.body.password = request.body.password.trim();
+        },
+        handler: user.signup,
+    });
 
 
-  /**
-  * Route for user kyc.
-  * Handles the user kyc.
-  */
-  route.post('/kyc/url', {
-    schema: validator.kycUrl,
-    onRequest: [
-      route.authenticate
-    ],
-    // preValidation: validateToken,
-    preHandler: async (request, reply) => {
-      const phoneNumber = request.body.phone_number.trim();
-      request.body.phone_number = `+91-${phoneNumber}`;
-    },
-    handler: user.getKycUrl,
-  });
+     /**
+     * Route for user kyc.
+     * Handles the user kyc.
+     */
+     route.post('/kyc/url', {
+        schema: validator.kycUrl,
+        preValidation: validateToken,
+        preHandler: async (request, reply) => {
+          const phoneNumber = request.body.phone_number.trim();
+          request.body.phone_number = `+91-${phoneNumber}`;
+        },
+        handler: user.getKycUrl,
+      });
 
-  /**
-   * Route for user profile.
-   * Handles the user profile
-   */
-  route.get('/profile', {
-    //schema: validator.profile,
-    onRequest: [
-      route.authenticate
-    ],
-    //  preValidation: validateToken,
+    /**
+     * Route for user profile.
+     * Handles the user profile
+     */
+      route.get('/profile', {
+          //schema: validator.profile,
+          preValidation:validateToken,
+        //   preHandler: async (request, reply) => {
+        //       request.body.email_id = request.body.email_id.trim();
+        //   },
+          handler: user.getProfile,
+      });
 
-    //   preHandler: async (request, reply) => {
-    //       request.body.email_id = request.body.email_id.trim();
-    //   },
-    handler: user.getProfile,
-  });
-
-  /**
-* Route for adding  user phone number.
-* Handles the user phone number update
-*/
-  route.post('/add/phone', {
-    //schema: validator.updatePhone,
-    onRequest: [
-      route.authenticate
-    ],
-    // preValidation: validateToken,
-    preHandler: async (request, reply) => {
-      request.body.phone_number = request.body.phone_number.trim();
-    },
-    handler: user.updatePhone,
-  });
+       /**
+     * Route for adding  user phone number.
+     * Handles the user phone number update
+     */
+       route.post('/add/phone', {
+        //schema: validator.updatePhone,
+        preValidation:validateToken,
+        preHandler: async (request, reply) => {
+            request.body.phone_number = request.body.phone_number.trim();
+        },
+        handler: user.updatePhone,
+    });
 
 
 
