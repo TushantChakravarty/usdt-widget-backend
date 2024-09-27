336fa4d97d12e5aa94abed694404442da4f06ec7
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-09-27 15:51:45 +0530

    Update index.js

--- src/utils/cron/index.js
+++ src/utils/cron/index.js
@@ -12,9 +12,9 @@ import { getQuotes, getQuotesOfframp } from "../../ApiCalls/usdtapicalls";
  * @returns {CronJob}
  * @see {@link https://www.npmjs.com/package/cron}
  */
-const Every1HourCronJob = new CronJob("0 * * * *", async () => {
+const Every1MinuteCronJob = new CronJob("*/1 * * * *", async () => {
     try {
-        console.log("Hourly cron job starting for quotes");
+        console.log("1-minute cron job starting for quotes");
         const body = {
             fromCurrency: "INR",
             toCurrency: "USDT",
@@ -31,17 +31,18 @@ const Every1HourCronJob = new CronJob("0 * * * *", async () => {
             }
         
         await getQuotes(body); 
-        await getQuotesOfframp(bodyOfframp)
-         // Make sure to await asynchronous functions
-        console.log("Hourly cron job end");
+        await getQuotesOfframp(bodyOfframp);
+        // Make sure to await asynchronous functions
+        console.log("1-minute cron job end");
     } catch (err) {
-        console.error(`HourlyCronJob error: ${err}`);  // Changed logger.error to console.error for simplicity
+        console.error(`1MinuteCronJob error: ${err}`);  // Changed logger.error to console.error for simplicity
     }
 }, null, true, "Asia/Kolkata");
 
 
+
 // export all cron jobs
 export default () => {
-    Every1HourCronJob;
+    Every1MinuteCronJob;
  
 };
