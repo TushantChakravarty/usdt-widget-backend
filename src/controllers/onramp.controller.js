9ebe83119f0dcc85c7c34d581e2ec2993d715aaf
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-10-29 15:35:50 +0530

    updates

--- src/controllers/onramp.controller.js
+++ src/controllers/onramp.controller.js
@@ -11,6 +11,7 @@ import { networks } from "../utils/networks.js";
 import { findOneAndUpdate, findRecord } from "../Dao/dao.js";
 import { createPayinBankRequest, generateToken } from "../ApiCalls/payhub.js";
 import { transferUSDT, tronWeb} from "../utils/tronUtils.js";
+import { AllNetworksData } from "../../blockchainData/network_data.js";
 
 const { User, Coin, OnRampTransaction, Usdt } = db;
 
@@ -42,7 +43,12 @@ export async function getAllOnRampTransaction(request, reply) {
 export async function onRampRequest(request, reply) {
     try {
       const { fromCurrency, toCurrency, chain, paymentMethodType, depositAddress, fromAmount, toAmount, rate } = request.body
-  
+      const verified = await verifyQuotes(request.body)
+      console.log("verify check",verified)
+      if(!verified)
+      {
+        return reply.status(400).send(responseMappingError(400, "Quote has changed. please try again"))
+      }
     //   if (!request.user.isKycCompleted) {
     //     return reply.status(500).send(responseMappingError(500, "Please complete your kyc"))
     //   }
@@ -390,6 +396,106 @@ export async function onRampRequest(request, reply) {
       console.log("on ramp verify", error.message)
       return error
       // return reply.status(500).send(responseMappingError(500, `internal server error`))
+    }
+  }
+
+  export async function verifyQuotes(details) {
+    try {
+      const { fromCurrency, toCurrency, fromAmount, chain, paymentMethodType, toAmount } = details
+      const dataNet = networks
+      let updatedData = []
+      const coinData = await Coin.findOne({
+        where: {
+          coinid: 54
+        }
+      })
+      
+      const networkData = AllNetworksData//await getAllNetworkData()
+      const filteredNetworks = networkData.filter(item => item.coinid == 54)
+    
+      let query ={
+        id:1
+      }
+      const usdt = await findRecord(Usdt,query)
+        if (coinData) {
+          dataNet.map((item) => {
+          const networkData = filteredNetworks.filter(Item => Item.networkId == item.chainId)
+          if (networkData[0]?.withdrawalFee) {
+            
+            updatedData.push({
+              ...item,
+              icon: coinData.coinIcon,
+              fee: networkData[0]?.withdrawalFee,
+              minBuy: networkData[0]?.minimumWithdrawal,
+              minBuyInRupee: usdt?.inrRate ? Math.ceil(Number(networkData[0]?.minimumWithdrawal) * usdt?.inrRate) : Math.ceil(networkData[0]?.minimumWithdrawal)          })
+          }
+        })
+      }
+      const minWithdrawl = updatedData.find((item)=> item.chainSymbol == chain)
+      if(minWithdrawl.minBuyInRupee>fromAmount)
+      return reply.status(500).send(responseMappingError(400, `Amount should be greater than ${minWithdrawl.minBuyInRupee}`))
+ 
+  
+      // const cachedData =await request.server.redis.get(`${fromCurrency}-${toCurrency}-${fromAmount}-${chain}-${paymentMethodType}`)
+  
+      // if(cachedData){
+      //   let data_cache = await JSON.parse(cachedData);
+      //   console.log(data_cache);
+      //   if(data_cache.data)
+      //   {
+      //     let updatedData = data_cache.data
+      //     updatedData.feeInUsdt = Number(data_cache?.data?.fees[0]?.gasFee)/Number(data_cache?.data?.rate)
+      //     return reply
+      //     .status(200)
+      //     .send(responseMappingWithData(200, "success", updatedData));
+      // }
+      // }
+  
+     
+      const TronData = updatedData.filter((item)=>item.chainSymbol == chain)
+      const platformfee = 0.0025
+      const onRampFee = Number(fromAmount)*platformfee
+      const toAmountUsdt = (Number(fromAmount)/usdt.inrRate) - ((Number(fromAmount)*platformfee)/usdt.inrRate) - TronData[0].fee
+     
+     const quote ={
+      "fromCurrency": fromCurrency,
+      "toCurrency": toCurrency,
+      "toAmount": toAmountUsdt.toFixed(2),
+      "fromAmount": fromAmount,
+      "rate": usdt.inrRate,
+      "fees": [
+        {
+            "type": "fiat",
+            "onrampFee":onRampFee ,
+            "clientFee": "0",
+            "gatewayFee": "0",
+            "gasFee": TronData[0].fee
+        }
+      ],
+      "feeInUsdt": TronData[0].fee
+    }
+    
+    console.log("quiotess",quote)
+    // const enter = await request.server.redis.set(`${fromCurrency}-${toCurrency}-${fromAmount}-${chain}-${paymentMethodType}`,JSON.stringify(quote), 'EX', 7200);
+      if(quote)
+      {
+        if(quote.toAmount==toAmount)
+        {
+          return true
+        }else{
+          return false
+        }
+     
+      }else{
+        return false
+    
+      }
+  
+    } catch (error) {
+      logger.error("user.controller.getQuotes", error.message)
+      console.log('user.controller.getQUotes', error.message)
+      return error
+  
     }
   }
   
