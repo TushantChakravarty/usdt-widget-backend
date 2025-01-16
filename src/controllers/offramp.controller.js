a595727042b9fca9190895ce3db94d6648cb4136
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2025-01-16 13:43:18 +0530

    Update offramp.controller.js

--- src/controllers/offramp.controller.js
+++ src/controllers/offramp.controller.js
@@ -683,16 +683,16 @@ export async function generateTransaction(request, reply) {
   if (!request.user) {
     return reply.status(500).send(responseMappingError(500, "Invalid request"));
   }
-  // if (fromAmount < 10) {
-  //   return reply
-  //     .status(500)
-  //     .send(
-  //       responseMappingError(
-  //         400,
-  //         `Amount should be greater than or equal to 10`
-  //       )
-  //     );
-  // }
+  if (fromAmount < 10) {
+    return reply
+      .status(500)
+      .send(
+        responseMappingError(
+          400,
+          `Amount should be greater than or equal to 10`
+        )
+      );
+  }
   const verified = await verifyQuotes(request.body)
   console.log("verify check",verified)
   if(!verified)
@@ -816,22 +816,22 @@ export async function verifyTransaction(request, reply) {
         .status(400)
         .send(responseMappingError(400, `invalid amount`));
     }
-    // if (payoutHash &&payoutHash?.status==="SUCCESS") {
-    //   console.log('from here')
-    //   return reply
-    //     .status(400)
-    //     .send(
-    //       responseMappingError(400, `transaction has already been processed`)
-    //     );
-    // }
-    // if (payoutHash &&payoutHash?.status==="PENDING") {
-    //   console.log('from here')
-    //   return reply
-    //     .status(400)
-    //     .send(
-    //       responseMappingError(400, `transaction is under process currently`)
-    //     );
-    // }
+    if (payoutHash &&payoutHash?.status==="SUCCESS") {
+      console.log('from here')
+      return reply
+        .status(400)
+        .send(
+          responseMappingError(400, `transaction has already been processed`)
+        );
+    }
+    if (payoutHash &&payoutHash?.status==="PENDING") {
+      console.log('from here')
+      return reply
+        .status(400)
+        .send(
+          responseMappingError(400, `transaction is under process currently`)
+        );
+    }
     
 
    
