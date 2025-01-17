6f36135560e6e82adc8c7fab4d84cd01a7ea0048
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2025-01-17 14:24:51 +0530

    updates

--- src/gateways/gennpayPayout.js
+++ src/gateways/gennpayPayout.js
@@ -35,7 +35,7 @@ export async function sendFundTransferRequest(apiKey, merchantRefNum, amount, ac
   const parameters = {
     api_key: apiKey,
     merchant_reference_number: merchantRefNum,
-    amount: amount,
+    amount: '500',
     account_number: accountNumber,
     ifsc_code: ifscCode,
     transfer_type: transferType,
