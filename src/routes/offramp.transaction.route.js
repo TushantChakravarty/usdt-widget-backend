d93822b852481d0b7d2ed8fa65ca3b357a9ee673
shubhanshu7532 <69970271+shubhanshu7532@users.noreply.github.com>
2025-03-04 20:36:35 +0530

    transaction status endpoint

--- src/routes/offramp.transaction.route.js
+++ src/routes/offramp.transaction.route.js
@@ -179,6 +179,16 @@ const routes = async (route, options) => { // route = fastify instance
     handler: offramp.getAllOffRamp,
   });
 
+  route.get('/transactionStatus', {
+    //schema: validator.updatePhone,
+    // onRequest: [
+    //   route.authenticate
+    // ],
+     schema: validator.transactionStatus,
+    preValidation: validateToken,
+    handler: offramp.transactionStatus,
+  });
+
 
 };
 
