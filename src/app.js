636a481d11521d9c24a977e8113ca043c1cfd80e
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2025-03-18 12:38:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -267,7 +267,7 @@ await server.get('/', async (request, reply) => {
     const state_code = request.headers['X-State-Code'] || 'Unknown';
     // Use state code in your application logic
     return reply.send({
-        message: 'Hello world.1',
+        message: 'Hello world.22',
         environment: process.env.NODE_ENV,
     })
 
